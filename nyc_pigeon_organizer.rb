dd9a33807ba3877e47900ff7e0a1e9d6f842a9b5
James Pier <jpier2012@gmail.com>
2019-04-12 13:40:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,13 +6,16 @@ def nyc_pigeon_organizer(data_hash)
   data_hash.each {|stat, stats|
   # stat = :color, :gender, :lives
     stats.each {|key, array|
+    # key = :purple, :grey, :white, :brown, :male, :female, "Subway", "Central Park", "Library", "City Hall"
       array.each {|element|
+      # data arrays
         if !name_list.include?(element)
-
+          name_list << element
+        end
       }
     }
   }
 
 
-
+  
 end
