ee89a3e3fea65a6f36133e471c2ad2f0fe32abb3
James Pier <jpier2012@gmail.com>
2019-04-12 13:49:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,13 +9,16 @@ def nyc_pigeon_organizer(data_hash)
     # key = :purple, :grey, :white, :brown, :male, :female, "Subway", "Central Park", "Library", "City Hall"
       array.each {|element|
       # data arrays
-        if !name_list.include?(element)
-          name_list << element
+        if !pigeon_list.keys.include?(element)
+          pigeon_list[element] = {
+            :color => [],
+            :gender => [],
+            :lives => []            
+          }
         end
+        pigeon_list[element][stat] << key.to_s 
       }
     }
   }
-
-
-
+  pigeon_list
 end
